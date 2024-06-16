d790e0c2de7913d3150c16b7a56c576b8dd80e42
pedro <pedrooliveiradevs@gmail.com>
2024-06-16 18:21:01 -0300

    Adicionando migracao

---
+++ FarmManagementSystem.Infra/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using FarmManagementSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FarmManagementSystem.Infra.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Farm> Farm { get; set; }
+        public DbSet<Location> Location { get; set; }
+        public DbSet<Crop> Crop { get; set; }
+        public DbSet<Animal> Animal { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
---
+++ FarmManagementSystem.Infra/Migrations/20240616211412_CriandoBancoDeDados.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FarmManagementSystem.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriandoBancoDeDados : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Farm",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Farm", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Animal",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FarmId = table.Column<int>(type: "int", nullable: false),
+                    Species = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Breed = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animal", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Animal_Farm_FarmId",
+                        column: x => x.FarmId,
+                        principalTable: "Farm",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Crop",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FarmId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Area = table.Column<double>(type: "float", nullable: false),
+                    CropStatus = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Crop", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Crop_Farm_FarmId",
+                        column: x => x.FarmId,
+                        principalTable: "Farm",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FarmId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Position = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Salary = table.Column<double>(type: "float", nullable: false),
+                    EmployeeStatus = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employee_Farm_FarmId",
+                        column: x => x.FarmId,
+                        principalTable: "Farm",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FarmId = table.Column<int>(type: "int", nullable: false),
+                    Latitude = table.Column<double>(type: "float", nullable: false),
+                    Longitude = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Location_Farm_FarmId",
+                        column: x => x.FarmId,
+                        principalTable: "Farm",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animal_FarmId",
+                table: "Animal",
+                column: "FarmId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Crop_FarmId",
+                table: "Crop",
+                column: "FarmId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_FarmId",
+                table: "Employee",
+                column: "FarmId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Location_FarmId",
+                table: "Location",
+                column: "FarmId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animal");
+
+            migrationBuilder.DropTable(
+                name: "Crop");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Location");
+
+            migrationBuilder.DropTable(
+                name: "Farm");
+        }
+    }
+}
